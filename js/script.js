c630d174f31ce43a92b941ae2b2a83a0b3b57d12
Cristian Cevasco <cristian.cevasco@gmail.com>
2026-06-28 20:04:49 +0200

    add logic to interact with ui

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ function getComputerChoice() {
 // Declare players' score variables
 let humanScore = 0;
 let computerScore = 0;
+let roundResult = "";
 
 
 // Logic to play one round
@@ -36,11 +37,17 @@ function playRound(humanChoice, computerChoice) {
 		if (computerChoice === "scissors") {
 			console.log("You win! Rock beats Scissors");
 			humanScore++;
+			roundResult = "win";
+			return { humanChoice, computerChoice, roundResult };
 		} else if (computerChoice === "paper") {
 			console.log("You lose! Paper beats Rock");
 			computerScore++;
+			roundResult = "lose";
+			return { humanChoice, computerChoice, roundResult };
 		} else {
 			console.log("It's a tie! Rock cannot beat Rock");
+			roundResult = "tie";
+			return { humanChoice, computerChoice, roundResult };
 		};
 	};
 
@@ -49,11 +56,17 @@ function playRound(humanChoice, computerChoice) {
 		if (computerChoice === "rock") {
 			console.log("You win! Paper beats Rock");
 			humanScore++;
+			roundResult = "win";
+			return { humanChoice, computerChoice, roundResult };
 		} else if (computerChoice === "scissors") {
 			console.log("You lose! Scissors beats Paper");
 			computerScore++;
+			roundResult = "lose";
+			return { humanChoice, computerChoice, roundResult };
 		} else {
 			console.log("It's a tie! Paper cannot beat Paper");
+			roundResult = "tie";
+			return { humanChoice, computerChoice, roundResult };
 		};
 	};
 
@@ -62,40 +75,83 @@ function playRound(humanChoice, computerChoice) {
 		if (computerChoice === "paper") {
 			console.log("You win! Scissors beats Paper");
 			humanScore++;
+			roundResult = "win";
+			return { humanChoice, computerChoice, roundResult };
 		} else if (computerChoice === "rock") {
 			console.log("You lose! Rock beats Scissors");
 			computerScore++;
+			roundResult = "lose";
+			return { humanChoice, computerChoice, roundResult };
 		} else {
 			console.log("It's a tie! Scissors cannot beat Scissors");
+			roundResult = "tie";
+			return { humanChoice, computerChoice, roundResult };
 		};
 	};
 };
 
 
-// Logic to play the game in a desired number of rounds
-function playGame() {
-	// Set rounds to 5
-	rounds = 5
+// Define buttons
+const button = document.querySelector("button");
+const buttonRock = document.querySelector(".buttonRock");
+const buttonPaper = document.querySelector(".buttonPaper");
+const buttonScissors = document.querySelector(".buttonScissors");
 
-	while (rounds > 0) {
-		// Store human and computer choices
-		const humanSelection = getHumanChoice();
-		const computerSelection = getComputerChoice();
-		// Print choices
-		console.log("╔ Your selection: " + humanSelection);
-		console.log("╚ Computer selection: " + computerSelection);
+// Define vars to show results and scores
+const userScorePoints = document.getElementById("userScore");
+const computerScorePoints = document.getElementById("computerScore");
+const computersPick = document.getElementById("computersPick");
+const showRoundResult = document.getElementById("roundResult");
 
-		playRound(humanSelection, computerSelection);
-		rounds--;
-	};
 
-	if (humanScore > computerScore) {
-		console.log(`» FINAL RESULTS » Congratulations! You win. Your score: ${humanScore}. Computer score: ${computerScore}.`)
+// Set mouseover event for buttons
+buttonRock.addEventListener("mouseover", () => {
+	buttonRock.style.backgroundColor = "yellow";
+	buttonRock.style.cursor = "pointer";
+});
+
+buttonRock.addEventListener("mouseout", () => {
+	buttonRock.style.backgroundColor = "";
+});
+
+buttonPaper.addEventListener("mouseover", () => {
+	buttonPaper.style.backgroundColor = "yellow";
+	buttonPaper.style.cursor = "pointer";
+});
+
+buttonPaper.addEventListener("mouseout", () => {
+	buttonPaper.style.backgroundColor = "";
+});
+
+buttonScissors.addEventListener("mouseover", () => {
+	buttonScissors.style.backgroundColor = "yellow";
+	buttonScissors.style.cursor = "pointer";
+});
+
+buttonScissors.addEventListener("mouseout", () => {
+	buttonScissors.style.backgroundColor = "";
+});
+
+
+// Set logic to play a round when an object button is pressed
+function clickHandler(humanChoice) {
+	// Store result from playRound();
+	result = playRound(humanChoice, getComputerChoice());
+	computersPick.textContent = result.computerChoice.toUpperCase();
+
+	if (result.roundResult === "win") {
+		userScorePoints.textContent = humanScore;
+		showRoundResult.textContent = "You win!";
+	} else if (result.roundResult === "lose") {
+		computerScorePoints.textContent = computerScore;
+		showRoundResult.textContent = "You lose!";
 	} else {
-		console.log(`» FINAL RESULTS » You lost! Computer score: ${computerScore}. Your score: ${humanScore}.`)
+		showRoundResult.textContent = "It's a tie!";
 	};
 };
 
 
-// Play Game
-playGame();
+// Set onclick event listeners for buttons
+buttonRock.onclick = () => clickHandler("rock");
+buttonPaper.onclick = () => clickHandler("paper");
+buttonScissors.onclick = () => clickHandler("scissors");
